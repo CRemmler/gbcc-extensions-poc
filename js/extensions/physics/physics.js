f68450f3af7dc6231eaf5bb4700209f0e290a67c
Carolyn Remmler <carolynremmler@C-L-MAC-4279.verse>
2018-02-19 07:04:20 -0600

    control physics groups

--- js/extensions/physics/physics.js
+++ js/extensions/physics/physics.js
@@ -24,14 +24,35 @@ Physics = (function() {
     $(".physics-controls").css("display","none");
     spanText = "<div id='physicsMenu'>";
     spanText +=       "<div class='leftControls'>"; //id='physicsDrawControls'>";
-    spanText +=         " <img src='js/extensions/physics/images/img5-old.png' id='physicsDrag'>"
+    
+    
+    spanText +=         " <img src='js/extensions/physics/images/a1.png' class='physics-drag purple hidden'>"
+    spanText +=         " <img src='js/extensions/physics/images/a2.png' class='physics-drag white'>"
+    spanText +=         " <img src='js/extensions/physics/images/a3.png' class='physics-line purple hidden'>"
+    spanText +=         " <img src='js/extensions/physics/images/a4.png' class='physics-line white'>"
+    spanText +=         " <img src='js/extensions/physics/images/a5.png' class='physics-circle purple hidden'>"
+    spanText +=         " <img src='js/extensions/physics/images/a6.png' class='physics-circle white'>"
+    spanText +=         " <img src='js/extensions/physics/images/a7.png' class='physics-triangle purple hidden'>"
+    spanText +=         " <img src='js/extensions/physics/images/a8.png' class='physics-triangle white'>"
+    spanText +=         " <img src='js/extensions/physics/images/a9.png' class='physics-group purple hidden'>"
+    spanText +=         " <img src='js/extensions/physics/images/a10.png' class='physics-group white'>"
+    spanText +=         " <img src='js/extensions/physics/images/a11.png' class='physics-target purple hidden'>"
+    spanText +=         " <img src='js/extensions/physics/images/a12.png' class='physics-target white'>"
+    
+    
+    
+    /*
+    spanText +=         " <img src='js/extensions/physics/images/img-2.png' id='physicsDrag'>"
     spanText +=         " <img src='js/extensions/physics/images/img6.png' id='physicsLine'>"
     spanText +=         " <img src='js/extensions/physics/images/img4.png' id='physicsCircle'>"
     spanText +=         " <img src='js/extensions/physics/images/img7.png' id='physicsTriangle'>"
     //spanText +=         " <img src='js/extensions/physics/images/img8.png' id='physicsQuad'>"
     spanText +=         " <img src='js/extensions/physics/images/img3.png' id='physicsGroup'>"
-    spanText +=         " <img src='js/extensions/physics/images/img2.png' id='physicsJoint'>"
+    //spanText +=         " <img src='js/extensions/physics/images/img2.png' id='physicsJoint'>"
     spanText +=         " <img src='js/extensions/physics/images/img1.png' id='physicsTarget'>"
+    */
+    
+    
     spanText +=       "</div>";
     spanText +=       "<div class='rightControls'>"; //"<div id='physicsStateControls'>";
     
@@ -43,22 +64,39 @@ Physics = (function() {
     spanText += "</div>";
     $(".netlogo-view-container").append(spanText);  
     
-    spanText =  "<div id='physicsSettings'>";
+    spanText =  "<div id='physicsSettings' class='hidden'>";
     spanText += "  <div class='leftControls'>";//"<span id='shapeSettings'>";
-    spanText += "    Id: <input class='objectId' type='text' value='123'>";
-    spanText += "    Type:<select class='objectType' style='background-color:white'><option value='2'>Dynamic</option><option value='0'>Static</option><option value='1'>Ghost</option></select>";
-    spanText += "    D:<input type='number' value='0.5'>";
-    spanText += "    R:<input type='number' value='0.2'>";
-    spanText += "    F:<input type='number' value='0.3'>";
+    spanText += "    <div id='dragModeSettings'>";
+    spanText += "      Id: <input class='objectId' type='text'>";
+    spanText += "      Color: <select id='color'>";
+    spanText += "        <option value='(none)'>(none)</option>";
+    spanText += "        <option value='#ff000032'>red</option>";
+    spanText += "        <option value='#ffa50032'>orange</option>";
+    spanText += "        <option value='#ffff0032'>yellow</option>";
+    spanText += "        <option value='#00ff0032'>green</option>";
+    spanText += "        <option value='#0000ff32'>blue</option>";
+    spanText += "        <option value='#80008032'>purple</option>";
+    spanText += "        <option value='(other)'>(other)</option>";
+    spanText += "      </select>";
+    spanText += "      D:<input type='number' id='density'>";
+    spanText += "      R:<input type='number' id='restitution'>";
+    spanText += "      F:<input type='number' id='friction'>";
+    spanText += "    </div>";
+    spanText += "    <div id='groupModeSettings' class='in-line-block'>";
+    spanText += "      Id: <input class='objectId' type='text' value='123'>";
+    spanText += "      Type:<select class='objectType' style='background-color:white'><option value='2'>Dynamic</option><option value='0'>Static</option><option value='1'>Ghost</option></select>";
+    spanText += "      Angle:<input type='number' id='angle'>";
+    spanText += "    </div>"
     spanText += "  </div>"; 
     spanText += "  <div class='rightControls'>";//"<span id='physicsTrash'>";
-    spanText += "    <i class='fa fa-trash-o' aria-hidden='true'></i>";
+    spanText += "    <i class='fa fa-trash-o' id='physicsDelete' aria-hidden='true'></i>";
     spanText += "  </div>";
     spanText += "</div>";
     
     $(".netlogo-view-container").append(spanText);  
     $("#physicsMenu").css("display", "none");
-    $("#physicsSettings").css("display", "none");
+    //$("#physicsSettings").css("display", "none");
+    //$("#physicsSettings").
     setupEventListeners();
   }
   
@@ -66,7 +104,11 @@ Physics = (function() {
     $("#physicsContainer").css("display","inline-block");
     $(".physics-controls").css("display","inline-block");
     $("#physicsMenu").css("display","inline-block");
+    $("#physicsMenu .purple").addClass("hidden");
+    $("#physicsMenu .white").removeClass("hidden");
+    
     updatePhysics("physicsOff");
+
   }
   
   function importPhysics(data) {
@@ -83,7 +125,6 @@ Physics = (function() {
   }
   
   function createObject(name, settings) {
-    //console.log("create object");
     var key, value;
     var parentId;
     var behavior = "dynamic";
@@ -96,6 +137,7 @@ Physics = (function() {
     var friction = 0.5;
     var restitution = 0.5;
     var objects = [];
+    var color = "(none)";
     var bodyCoords, fixtureCoords;
     for (var i=0; i<settings.length; i++) {
       key = settings[i][0];
@@ -111,7 +153,11 @@ Physics = (function() {
         case "friction": friction = value; break;
         case "restitution": restitution = value; break;
         case "objects": objects = value; break;
+        case "color": color = value; break;
       }
+    }
+    if (typeof color === "object") {
+      color = rgbToHex(color);
     }
     parentId = name+"parent";
     if (shape === "line") {
@@ -143,7 +189,8 @@ Physics = (function() {
         "bodyCoords": bodyCoords, 
         "fixtureCoords": fixtureCoords, 
         "shape": shape, 
-        "settings": [density, friction, restitution]
+        "settings": [density, friction, restitution],
+        "color": color
       });  
       Physicsb2.addFixtureToBody({
         "fixtureId": name, 
@@ -153,6 +200,32 @@ Physics = (function() {
 
     Physicsb2.redrawWorld();
     
+  }
+  
+  function rgbToHex(color) {
+    var result;
+    var r = color[0];
+    var g = color[1];
+    var b = color[2];
+    if (color[3] != undefined) {
+      var a = color[3];
+      a = a.toString(16);
+      if (a.length === 1) { a = "0" + a; }
+    }
+    r = r.toString(16);
+    g = g.toString(16);
+    b = b.toString(16);
+    if (r.length === 1) { r = "0" + r; }
+    if (g.length === 1) { g = "0" + g; }
+    if (b.length === 1) { b = "0" + b; }
+    result = r+g+b;
+    if (color[3] != undefined) {
+      var a = color[3];
+      a = a.toString(16);
+      if (a.length === 1) { a = "0" + a; }
+      result = result + a;
+    }
+    return "#"+result;
   }
   
   function connectToObject(who, name) {
@@ -176,7 +249,7 @@ Physics = (function() {
   }
   
   function getObjects() {
-    console.log("get objects");
+    //console.log("get objects");
     var objectsList = [];
     var bodies = Physicsb2.getAllBodies();
     var object, name, settings, fixtures, fixtureType, bodyType, position, angle, radius;
@@ -203,9 +276,9 @@ Physics = (function() {
         }
         if (k === "b2PolygonShape") { fixtureType = "polygon"; }
       }
-      console.log("name:"+name+" bodyType:"+bodyType+" fixtureType:"+ fixtureType+" position:"+position.x+","+position.y+" angle:"+angle);
+      //console.log("name:"+name+" bodyType:"+bodyType+" fixtureType:"+ fixtureType+" position:"+position.x+","+position.y+" angle:"+angle);
       if (fixtureType === "circle") {
-        console.log("radius: "+radius);
+        //console.log("radius: "+radius);
       }
     }
   }
@@ -228,32 +301,58 @@ Physics = (function() {
       startWorld();
       $(this).addClass("hidden");
       $("#physicsPause").removeClass("hidden");
+      $("#physicsMenu .leftControls").removeClass("selected");
     });
     $("#physicsMenu").on("click", "#physicsPause", function() {
       stopWorld();
       $(this).addClass("hidden");
       $("#physicsPlay").removeClass("hidden");
+      
+      $("#physicsMenu .leftControls").addClass("selected");
+    });
+    $("#physicsSettings").on("click", "#physicsDelete", function() {
+      console.log("delete it");
+      Physicsb2.deleteSelected();
     });
     $(".netlogo-view-container").css("background-color","transparent"); 
-    assignDrawButtonMode("Drag"); 
-    assignDrawButtonMode("Line"); 
-    assignDrawButtonMode("Circle"); 
-    assignDrawButtonMode("Triangle"); 
-    assignDrawButtonMode("Quad"); 
-    assignDrawButtonMode("Group"); 
-    assignDrawButtonMode("Joint"); 
-    assignDrawButtonMode("Target");
+    assignDrawButtonMode("drag"); 
+    assignDrawButtonMode("line"); 
+    assignDrawButtonMode("circle"); 
+    assignDrawButtonMode("triangle"); 
+    //assignDrawButtonMode("quad"); 
+    assignDrawButtonMode("group"); 
+    //assignDrawButtonMode("joint"); 
+    assignDrawButtonMode("target");
+    assignSettings("color");
+    assignSettings("density");
+    assignSettings("restitution");
+    assignSettings("friction");
+    assignSettings("objectId");
+    
+
   }
   
-  function assignDrawButtonMode(name) {
-    $("#physicsMenu .leftControls").on("click", "#physics"+name, function() {
-      if ($("#physicsMenu").hasClass("selected")) {
-        mode = name.toLowerCase();
-        $("#physicsMenu img.selected").removeClass("selected");
-        $(this).addClass("selected");
-        if (mode === "drag") {  
-          Physicsb2.triggerDragMode(); 
-        }
+  function assignSettings(setting) {
+    $("#physicsSettings .leftControls").on("change", "#"+setting, function () {
+      console.log("setting " + setting + " changed");
+      console.log($(this).val());
+      var value = $(this).val();
+      var fixtureId = $(".objectId").val();
+      if (["color","density","restitution","friction"].indexOf(setting) > -1) {
+        Physicsb2.updateFixture(fixtureId, setting, value);
+      }
+    });
+  }
+  
+  function assignDrawButtonMode(buttonMode) {
+    $("#physicsMenu .leftControls").on("click", ".physics-"+buttonMode, function() {
+      if ($("#physicsMenu .leftControls").hasClass("selected")) {
+        $("#physicsMenu .purple").addClass("hidden");
+        $("#physicsMenu .white").removeClass("hidden");
+        $(".physics-"+buttonMode+".purple").removeClass("hidden");
+        $(".physics-"+buttonMode+".white").addClass("hidden");
+        mode = buttonMode;
+        Physicsb2.triggerModeChange(mode);
       }
     });
   }
@@ -273,18 +372,23 @@ Physics = (function() {
       $("#physicsContainer").addClass("selected");
       $("#physicsPlay").removeClass("hidden");
       $("#physicsMenu .rightControls i:not(#physicsPause)").removeClass("hidden");
-      $("#physicsMenu").addClass("selected");
+      //$("#physicsMenu").addClass("selected");
+      $("#physicsMenu div").addClass("selected");
+      
       universe.repaint();
       Physicsb2.drawDebugData();
-
-      $("#physicsDrag").click();
+      $(".physics-drag").click()
     } else {
       $("#physicsOn").removeClass("selected");
       $("#physicsOff").addClass("selected");
       $("#physicsContainer").removeClass("selected");
       $("#physicsMenu").removeClass("selected");
       $("#physicsMenu .rightControls i").addClass("hidden");
+      
+      $("#physicsMenu div").removeClass("selected");
+      
       $("#physicsMenu img.selected").removeClass("selected");
+      $(".physics-drag").click()
     }
     $("#physicsSettings").addClass("hidden");
   }
